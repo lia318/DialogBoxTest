3f20d777efa6b8026b294409199de7ecf0cedb31
sojung kim <sojung kim>
2017-05-29 19:14:30 +0900

    #5 CheckBox

--- app/src/main/java/kr/hs/lia318e_mirim/dialogboxtest/MainActivity.java
+++ app/src/main/java/kr/hs/lia318e_mirim/dialogboxtest/MainActivity.java
@@ -33,13 +33,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 butDialog.setText(itemarr[i]);
             }
         });*/
+
         // 라디오 버튼 목록
-        dialog.setSingleChoiceItems(itemarr, 0, new DialogInterface.OnClickListener() {
+        /*dialog.setSingleChoiceItems(itemarr, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
                 butDialog.setText(itemarr[i]);
             }
-        }); // (배열이름, 배열 인덱스 번호, 이벤트)
+        }); // (배열이름, 배열 인덱스 번호, 이벤트) */
+
+        // 체크박스 목록 : 선택 여부 배열(boolean 형태)
+        boolean[] checkedItems = {true, false, true, false, true, false, true };
+        // 사용할 배열 크기에 맞춰서 작성
+        // 사용자에 따라 체크(true)값 설정
+        dialog.setMultiChoiceItems(itemarr, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i, boolean b) { // i : 선택된 인덱스 번호 / b : 항목 클릭 시 t or f
+                if(b){
+                    butDialog.setText(itemarr[i]);
+                } // end of if
+            }
+        }); // (배열이름, 배열이름, 인터페이스 이름)
+
         dialog.setPositiveButton("Ok", null); // null => 이벤트 발생x
         dialog.show(); // 꼭 설정하기
     }
